7cb6d2447d821e68d8de620b209db525fcb63b2a
islamm88 <142352912+islamm88@users.noreply.github.com>
2024-12-24 14:17:16 +0200

    Add files via upload

--- Jss/Sett.js
+++ Jss/Sett.js
@@ -1,73 +1,69 @@
 function Track_cer(event) {
-    
-    event.preventDefault();  //ok
-
-    const Form_Track_Cer = document.getElementById("Form_Track_Cer"); // Get the form element  //ok
+    event.preventDefault();
+    const Form_Track_Cer = document.getElementById("Form_Track_Cer");
 
     // Create unique section container
     const Div_Track_create = document.createElement("div");
-    const TrackId = `section-${Date.now()}`; // Generate a unique ID for the section
+    const TrackId = `section-${Date.now()}`;
     Div_Track_create.className = "Track_Cer";
-    Div_Track_create.id = "TrackId";
-    // Div_Track_create.style.border = "1px solid red";
+    Div_Track_create.id = TrackId;
     Div_Track_create.style.display = "flex";
     Div_Track_create.style.flexDirection = "column";
 
-
     // Create section label
     const label_Track = document.createElement("label");
-    label_Track.htmlFor = "Track-name";
+    const id_input = `id-${Date.now()}`;
+    label_Track.htmlFor = id_input;
     label_Track.innerText = "Track Name:";
-    // label_Track.id = "label_Track" ;
 
     // Create section input
     const input_track = document.createElement("input");
     input_track.type = "text";
-    input_track.placeholder = "Add Track Name ";
+    input_track.placeholder = "Add Track Name";
+    input_track.id = id_input;
 
-    // Create 'Add Certificate' button
-    const button_commit = document.createElement("button");
-    button_commit.className = "add";
-    // button_commit.innerHTML = "<i class='fa-solid fa-square-plus'></i>";
-    // button_commit.onclick = "submit"; 
-    // button_commit.style.width = "40px";
-    // button_commit.style.display = "inline"
-
-
+    // Create button to add certificates
     const button_cer = document.createElement("button");
     button_cer.className = "add-cer";
     button_cer.innerHTML = "Certificate <i class='fa-solid fa-square-plus'></i>";
-    button_cer.onclick = (e) => cer(e, sectionId); // Pass section ID to the `cer` function
+    button_cer.onclick = (e) => cer(e, TrackId);
 
     // Append elements to section
     Form_Track_Cer.append(Div_Track_create);
-    Div_Track_create.append(label_Track, input_track, button_commit, button_cer );
-
+    Div_Track_create.append(label_Track, input_track, button_cer);
 }
 
 function cer(event, sectionId) {
     event.preventDefault();
+    const sectionContainer = document.getElementById(sectionId);
 
-    const sectionContainer = document.getElementById(sectionId); // Get the specific section container
+    if (!sectionContainer) {
+        console.error(`Section with ID "${sectionId}" not found.`);
+        return;
+    }
 
     // Create certificate container
     const cer_div_create = document.createElement("div");
-    cer_div_create.className = "ncer";
+    cer_div_create.className = "divcer";
     cer_div_create.style.margin = "10px 0px";
+    cer_div_create.style.border = "1px solid var(--num1-color)";
+    cer_div_create.style.borderRadius = "22px";
+    cer_div_create.style.padding = "5px" ;
+    cer_div_create.style.display = "flex";
+    cer_div_create.style.flexDirection = "column";
+
 
     // Create file input for certificate image
     const inp_cer_img = document.createElement("input");
     inp_cer_img.className = "cer-img";
     inp_cer_img.type = "file";
     inp_cer_img.name = "cer-img";
-    inp_cer_img.title= "attach your certificate image";
-    
-
+    inp_cer_img.title = "Attach your certificate image";
 
     // Create label for certificate title
     const lab_cer = document.createElement("label");
     lab_cer.htmlFor = "cer-name";
-    lab_cer.innerText = "Certificte Name :";
+    lab_cer.innerText = "Certificate Name:";
 
     // Create text input for certificate title
     const inp_cer = document.createElement("input");
@@ -92,6 +88,9 @@ function cer(event, sectionId) {
     sectionContainer.append(cer_div_create);
     cer_div_create.append(inp_cer_img, lab_cer, inp_cer, lab_desc, area_cer);
 }
+
+
+
 
 function sec_pro(event)
 {
